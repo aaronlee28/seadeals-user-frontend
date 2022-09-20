3d6a4779e43e09b8a7cdd4d8c7128f96be970edf
fikra.ramadhan <fikra.ramadhan@shopee.com>
2022-09-20 22:41:28 +0700

    feat: add utils for format sold count

---
+++ src/utils/product.js
@@ -0,0 +1,12 @@
+const formatSoldCount = (count) => {
+  if (count >= 10000) {
+    return '10RB+ Terjual';
+  }
+  if (count >= 1000) {
+    const formatCount = Math.round((count / 1000) * 100) / 100;
+    return `${formatCount}RB Terjual`;
+  }
+  return `${count} Terjual`;
+};
+
+export default formatSoldCount;
