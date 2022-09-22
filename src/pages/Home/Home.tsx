f5777e09ff54c9f056b56a5e8f354af448f6a5e2
yohan.wijaya <yohan.wijaya@shopee.com>
2022-09-22 11:47:13 +0700

    fix(Configuration) fixed useLogout feature & style layouting

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Navbar from '../../layouts/Navbar/Navbar';
-import Footer from '../../layouts/Footer/Footer';
 
 import './Home.scss';
 
 const Home = () => (
   <div className="home_container">
-    <Navbar />
     <div className="home_content">...</div>
-    <Footer />
   </div>
 );
 
