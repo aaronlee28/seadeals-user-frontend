b6bf18d19a9d68cd498d85868c03d841efb50be7
fikra.ramadhan <fikra.ramadhan@shopee.com>
2022-10-13 16:51:39 +0700

    fix: fixing navbar

--- src/layouts/Navbar/NavbarRightContent.tsx
+++ src/layouts/Navbar/NavbarRightContent.tsx
@@ -1,27 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
+import useAuth from '../../hooks/useAuth';
+import { ReactComponent as IconHeart } from '../../assets/svg/icon_heart.svg';
+import Users from '../../api/users';
+import { ReactComponent as IconChevron } from '../../assets/svg/icon_chevron_right.svg';
 
 const NavbarRightContent = () => {
+  const [user, setUser] = useState({
+    avatar_url: '',
+    username: '',
+
+  });
+
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+
   const goToRegisterPage = () => {
-    window.location.href = '/register';
+    navigate('/register');
   };
+
   const goToLoginPage = () => {
-    window.location.href = '/login';
+    navigate('/login');
   };
+
+  const getUser = async () => {
+    await Users.GetUserProfile()
+      .then((resp) => {
+        setUser(resp.data.data);
+      })
+      .catch((err) => err);
+  };
+
+  const openDropdown = () => {
+    console.log('OPEN DROPDOWN');
+  };
+
+  console.log(auth);
+
+  useEffect(() => {
+    if (auth.user) {
+      getUser().then();
+    }
+  }, [auth.user]);
 
   return (
     <div className="right_content">
-      <Button
-        buttonType="primary"
-        text="Daftar"
-        handleClickedButton={goToRegisterPage}
-      />
-      <Button
-        buttonType="primary"
-        text="Masuk"
-        handleClickedButton={goToLoginPage}
-      />
+      {
+        !auth.user
+        && (
+          <div className="buttons">
+            <Button
+              buttonType="primary"
+              text="Daftar"
+              handleClickedButton={goToRegisterPage}
+            />
+            <Button
+              buttonType="primary"
+              text="Masuk"
+              handleClickedButton={goToLoginPage}
+            />
+          </div>
+        )
+      }
+      {
+        auth.user
+        && (
+          <div className="buttons">
+            <Button
+              buttonType="plain"
+              iconUrl={IconHeart}
+              iconName="favorite"
+              handleClickedButton={goToLoginPage}
+            />
+            <div className="profile">
+              <img
+                className="image"
+                src={user.avatar_url}
+                alt={user.username}
+              />
+              <Button
+                buttonType="plain"
+                text={user.username}
+                iconUrl={IconChevron}
+                iconName="chevron"
+                handleClickedButton={openDropdown}
+              />
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 };
