dee4055f10d66d6bd85dcc2c2520749b79394f7d
aaron.lee <aaron.lee@shopee.com>
2022-10-19 14:05:18 +0700

    feat(review) create review page

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7bAB1GNG0FKlqV2Ud5XPu7aB2kh4pGCc",
-  authDomain: "bucket-seadeals.firebaseapp.com",
-  projectId: "bucket-seadeals",
-  storageBucket: "bucket-seadeals.appspot.com",
-  messagingSenderId: "336103547465",
-  appId: "1:336103547465:web:31cbd26d343555bf379598"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
