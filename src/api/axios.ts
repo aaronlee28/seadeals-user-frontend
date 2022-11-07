9f68d1a3c4c727df23040dd5a9acfe705cb6bdc3
aaron.lee <aaron.lee@shopee.com>
2022-11-07 15:04:09 +0700

    deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080';
+const baseURL = 'https://seadeals-backend.herokuapp.com/';
 
 export default axios.create({
   baseURL,
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import moment from 'moment-timezone';
 import { store } from './app/store';
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <Provider store={store}>
         <Routes>
@@ -23,5 +23,5 @@ root.render(
         </Routes>
       </Provider>
     </AuthProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
